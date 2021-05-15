34fafb986b0d2d2401765bc0a7962c7547488efe
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 13:29:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,18 +4,17 @@ import discord
 client = discord.Client()
 
 BOT_TOKEN = "hogehoge"
-CHANNEL_ID = 817359255703257098
+CHANNEL_ID = 797040818794921984
 channel_sent = None
 
-@tasks.loop(minutes=120)
-    channel_sent = client.get_channel(CHANNEL_ID)
+@tasks.loop(seconds=10)
     async def send_message_every_120min():
     await channel_sent.send("!d bump")
 
 @client.event
 async def on_ready():
     global channel_sent 
-    channel_sent = client.get_channel(any_channel_id)
+    channel_sent = client.get_channel(797040818794921984)
     send_message_every_120min.start()
     
     client.run(BOT_TOKEN)
