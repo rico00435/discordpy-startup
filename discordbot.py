6ea40b29dfd44cf73682486030294f50c0888fbe
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 15:06:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,23 @@
-from discord.ext import commands
+from discord.ext import commands , tasks
 import os
 import traceback
+import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
+
+channel_sent = None
+@tasks.loop(seconds=10)
+async def send_message_every_10sec():
+    await channel_sent.send("test")
+
+@client.event
+async def on_ready():
+    global channel_sent 
+    channel_sent = client.get_channel(797040818794921984)
+    send_message_every_10sec.start()
 
 @bot.event
 async def on_command_error(ctx, error):
