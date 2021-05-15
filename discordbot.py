cea44cfd2e1a41a5ed91c289c70fe9375803c8ba
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 14:01:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,21 @@
-import discord
-import asyncio
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    asyncio.ensure_future(greeting_gm())
 
-async def greeting_gm():
-    await client.send_message(channel, 'おはよう')
-    await asyncio.sleep(10)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-client.run(token)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
