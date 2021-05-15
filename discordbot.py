bc2b178a3f751d2b038e45d9bfb24c992ddf07a1
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 13:40:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,21 @@
-from discord.ext import tasks
-import discord
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-channel_sent = None
 
-@tasks.loop(seconds=10)
-    async def send_message_every_120min():
-    await channel_sent.send("わわわ!d わわわbump")
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-@client.event
-async def on_ready():
-    global channel_sent 
-    channel_sent = client.get_channel(797040818794921984)
-    send_message_every_120min.start()
-    
-client.run("hogehogetoken")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
