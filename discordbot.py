c9d46e01d274f80bc6c504f6a0918b60b0ac82dd
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 15:08:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ import os
 import traceback
 import discord
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
@@ -18,17 +17,5 @@ async def on_ready():
     global channel_sent 
     channel_sent = client.get_channel(797040818794921984)
     send_message_every_10sec.start()
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 
 bot.run(token)
