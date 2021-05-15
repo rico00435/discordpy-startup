39607e5409e45f336b9ac7a5f880849b359e747a
rico00435 <84215895+rico00435@users.noreply.github.com>
2021-05-15 13:51:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,18 +18,5 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-client = discord.Client()
-    
-    channel_sent = None
-@tasks.loop(seconds=10)
-async def send_message_every_10sec():
-    await channel_sent.send("10秒経ったよ")
-
-@client.event
-async def on_ready():
-    global channel_sent 
-    channel_sent = client.get_channel(797040818794921984)
-    send_message_every_10sec.start() 
 
 bot.run(token)
